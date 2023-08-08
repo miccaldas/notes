b42c9b5255068cc163f1ecab9009aa18898c83a4
micaldas <micaldas@mailfence.com>
2023-08-09 00:00:40 +0100

    09-08-2023 commit

--- notes/old_search.py
+++ notes/old_search.py
@@ -1,5 +1,5 @@
 """
-Module to do searches in db and, possibly, bookmarks.
+Module to do searches in db and present the notes from files.
 """
 import os
 import pickle
@@ -20,49 +20,9 @@ def type_watch(source, value):
 snoop.install(watch_extras=[type_watch])
 
 
-# @snoop
-def bks(qry):
-    """
-    In case the user chooses to add to the db's
-    results those of 'bkmks', this function runs.
-    """
-    term = Terminal()
-    query = "SELECT id, title, comment, link, k1, k2, k3, tempo FROM bkmks WHERE MATCH(title, comment, k1, k2, k3) AGAINST (%s) ORDER BY tempo"
-
-    answers = [qry]
-    try:
-        conn = connect(host="localhost", user="mic", password="xxxx", database="bkmks")
-        cur = conn.cursor()
-        cur.execute(query, answers)
-        records = cur.fetchall()
-    except Error as e:
-        print(f"DB connection failed with error {e}")
-    finally:
-        if conn:
-            conn.close()
-
-    for record in records:
-        record = (
-            term.bold_lemonchiffon2(str(record[0])),
-            term.bold_yellow4(record[1]),
-            term.bold_lemonchiffon2(record[2]),
-            term.bold_lightcoral(record[3]),
-            term.bold_tan1("@")
-            + term.bold_yellow4(f"{record[4]}")
-            + term.bold_tan1(" @")
-            + term.bold_yellow4(f"{record[5]}")
-            + term.bold_tan1(" @")
-            + term.bold_yellow4(f"{record[6]}"),
-            term.bold_lemonchiffon2(record[7].strftime("%d-%m-%Y_%H:%M")),
-        )
-        for line in record:
-            print("\n".join(term.wrap(line, width=160)))
-        print("\n")
-
-
 @click.command()
 @click.argument("qry")
-@click.option("-bk", "--bkmk", is_flag=False, flag_value="bookmark", default="n")
+@click.option("-bk", "--bkmk", is_flag=False, flag_value="bookmark", default="y")
 # @snoop
 def search(qry, bkmk):
     """
@@ -105,11 +65,6 @@ def search(qry, bkmk):
             print("\n".join(term.wrap(line, width=160)))
         subprocess.run(cmd, shell=True)
         print("\n")
-    sep = "#" * 40
-    print(term.bisque4(f"{sep} BOOKMARKS {sep}\n"))
-
-    if bkmk == "y":
-        bks(qry)
 
 
 if __name__ == "__main__":
--- notes/search.py
+++ notes/search.py
@@ -1,5 +1,5 @@
 """
-Module to do searches in db and present the notes from files.
+Module to do searches in db and, possibly, bookmarks.
 """
 import os
 import pickle
@@ -18,6 +18,46 @@ def type_watch(source, value):
 
 
 snoop.install(watch_extras=[type_watch])
+
+
+# @snoop
+def bks(qry):
+    """
+    In case the user chooses to add to the db's
+    results those of 'bkmks', this function runs.
+    """
+    term = Terminal()
+    query = "SELECT id, title, comment, link, k1, k2, k3, tempo FROM bkmks WHERE MATCH(title, comment, k1, k2, k3) AGAINST (%s) ORDER BY tempo"
+
+    answers = [qry]
+    try:
+        conn = connect(host="localhost", user="mic", password="xxxx", database="bkmks")
+        cur = conn.cursor()
+        cur.execute(query, answers)
+        records = cur.fetchall()
+    except Error as e:
+        print(f"DB connection failed with error {e}")
+    finally:
+        if conn:
+            conn.close()
+
+    for record in records:
+        record = (
+            term.bold_lemonchiffon2(str(record[0])),
+            term.bold_yellow4(record[1]),
+            term.bold_lemonchiffon2(record[2]),
+            term.bold_lightcoral(record[3]),
+            term.bold_tan1("@")
+            + term.bold_yellow4(f"{record[4]}")
+            + term.bold_tan1(" @")
+            + term.bold_yellow4(f"{record[5]}")
+            + term.bold_tan1(" @")
+            + term.bold_yellow4(f"{record[6]}"),
+            term.bold_lemonchiffon2(record[7].strftime("%d-%m-%Y_%H:%M")),
+        )
+        for line in record:
+            print("\n".join(term.wrap(line, width=160)))
+        print("\n")
 
 
 @click.command()
@@ -65,6 +105,11 @@ def search(qry, bkmk):
             print("\n".join(term.wrap(line, width=160)))
         subprocess.run(cmd, shell=True)
         print("\n")
+    sep = "#" * 40
+    print(term.bisque4(f"{sep} BOOKMARKS {sep}\n"))
+
+    if bkmk == "y":
+        bks(qry)
 
 
 if __name__ == "__main__":
